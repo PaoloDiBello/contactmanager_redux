28cc8b1a3c31c69f6b664cfa5f6220367de44846
PaoloDiBello <dibellopol@gmail.com>
2019-03-23 22:41:23 +0100

    first-commit

---
+++ src/store.js
@@ -0,0 +1,39 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {
+  contacts: [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      phone: '555-555-5555'
+    },
+    {
+      id: 2,
+      name: 'Karen Williams',
+      email: 'karen@gmail.com',
+      phone: '444-444-4444'
+    },
+    {
+      id: 3,
+      name: 'Henry Johnson',
+      email: 'henry@gmail.com',
+      phone: '333-333-333'
+    }
+  ]
+}
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
